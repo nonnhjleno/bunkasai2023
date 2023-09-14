4c9e7e5760c17b179e47a16ca35a29096e2e0fa1
nonnhjleno <taaaisei999@gmail.com>
2023-09-14 16:39:04 +0900

    username検索

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +13,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $result = User::find(1);
+        return response()->json($result);
     }
 
     /**
@@ -26,9 +28,34 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request)
     {
-        //
+        $query = $request->input('username');
+
+        // 全角空白を半角スペースに変換
+        $query = str_replace('　', ' ', $query);
+
+        // スペースでキーワードを分割
+        $keywords = explode(' ', $query);
+
+        if (count($keywords) === 1) {
+            // キーワードが1つの場合、完全一致検索を行う
+            $users = User::where('username', $query)->get();
+        } else {
+            // キーワードが複数ある場合、AND検索を行う
+            $users = User::where(function ($query) use ($keywords) {
+                foreach ($keywords as $keyword) {
+                    $query->orWhere('username', 'like', '%' . $keyword . '%');
+                }
+            })->get();
+        }
+
+        if ($users->isEmpty()) {
+            // 部分一致で検索
+            $users = User::where('username', 'like', '%' . $query . '%')->get();
+        }
+
+        return response()->json($users);
     }
 
     /**
